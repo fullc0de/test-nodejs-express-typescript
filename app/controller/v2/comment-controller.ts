7380f5c845824ee37a7001531d7eb8975ea735ed
Heath Hwang <heath.supergene@gmail.com>
2019-12-27 19:12:53 +0800

    - add a test code for using the orm manager

--- app/controller/v2/comment-controller.ts
+++ app/controller/v2/comment-controller.ts
@@ -2,11 +2,17 @@ import { CommentController as V1 } from "../v1/comment-controller";
 import {HttpRequest, HttpResponse} from "../interfaces";
 import {Employer} from "../../model/Employer";
 import axios from "axios";
+import {getManager} from "typeorm";
+import {Employee} from "../../model";
 
 export class CommentController extends V1 {
     public async test(request: HttpRequest): Promise<HttpResponse> {
         // let aaa = await axios.get('http://localhost:5000/wow')
         // console.log(`axios response = ${aaa.data}`)
+
+        const manager = getManager()
+        const employee = await manager.findOne(Employee, 1)
+        console.log(`employee name = ${employee?.name}`)
 
         const employer = await Employer.findOne(1)
         if (employer == undefined) {
