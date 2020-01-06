f9ed1b43d0fec11d071f4d68d6c178282b34227d
Heath Hwang <heath.supergene@gmail.com>
2020-01-06 17:25:57 +0800

    - add a config file for circle CI

--- app/index.ts
+++ app/index.ts
@@ -1,32 +1,27 @@
-import express, {Router} from "express"
-
-import { myContainer } from "./inversify.config"
-import { TYPES } from "./types"
-import { Warrior } from "./interfaces"
+import express, {Router} from "express";
 import {createConnections, getConnectionOptions} from "typeorm";
-import router from "./router"
+import router from "./router";
 
-const app: express.Application = express()
-const ninja = myContainer.get<Warrior>(TYPES.Warrior);
+const app: express.Application = express();
 
 (async () => {
-    let postgresOpts = await getConnectionOptions("default")
-    let mysqlOpts = await getConnectionOptions("mysqldb")
-    const conns = await createConnections([postgresOpts, mysqlOpts])
+    let postgresOpts = await getConnectionOptions("default");
+    let mysqlOpts = await getConnectionOptions("mysqldb");
+    const conns = await createConnections([postgresOpts, mysqlOpts]);
     conns.forEach(conn => {
-        console.log(`conn name = ${conn.name}`)
-    })
+        console.log(`conn name = ${conn.name}`);
+    });
 })().catch(e => {
     // Deal with the fact the chain failed
-    console.log(e)
+    console.log(e);
 });
 
-app.use(router)
+app.use(router);
 app.use('*', (req, res, next) => {
-    res.status(404).send('NOT FOUND')
-})
+    res.status(404).send('NOT FOUND');
+});
 
 app.listen(3000, function() {
-    console.log('Example app listening on port: 3000!')
-})
+    console.log('Example app listening on port: 3000!');
+});
 
